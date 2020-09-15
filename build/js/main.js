69653668c122da006b209643a1a478ea4c440ce5
Pridecoo <pridecoo@gmail.com>
2020-09-15 12:52:43 +0300

    fixed few stuffs

--- build/js/main.js
+++ build/js/main.js
@@ -17,20 +17,28 @@ $(function () {
   });
 
 
-  $(window).scroll(() => { 
-    // Distance from top of document to top of footer.
-    topOfFooter = $('.footer').position().top;
-    // Distance user has scrolled from top, adjusted to take in height of sidebar (570 pixels inc. padding).
-    scrollDistanceFromTopOfDoc = $(document).scrollTop() + 570;
-    // Difference between the two.
-    scrollDistanceFromTopOfFooter = scrollDistanceFromTopOfDoc - topOfFooter;
+  // $(window).scroll(() => { 
+  //   // Distance from top of document to top of footer.
+  //   topOfFooter = $('.footer').position().top;
+  //   // Distance user has scrolled from top, adjusted to take in height of sidebar (570 pixels inc. padding).
+  //   scrollDistanceFromTopOfDoc = $(document).scrollTop() + 570;
+  //   // Difference between the two.
+  //   scrollDistanceFromTopOfFooter = scrollDistanceFromTopOfDoc - topOfFooter;
   
-    // If user has scrolled further than footer,
-    // pull sidebar up using a negative margin.
-    if (scrollDistanceFromTopOfDoc > topOfFooter) {
-      $('.banner__logo').css('margin-top',  0 - scrollDistanceFromTopOfFooter);
-    } else  {
-      $('.banner__logo').css('margin-top', 0);
+  //   // If user has scrolled further than footer,
+  //   // pull sidebar up using a negative margin.
+  //   if (scrollDistanceFromTopOfDoc > topOfFooter) {
+  //     $('.banner__logo').css('margin-top',  0 - scrollDistanceFromTopOfFooter);
+  //   } else  {
+  //     $('.banner__logo').css('margin-top', 0);
+  //   }
+  // });
+
+  $(window).scroll(function(event) {
+    let scroll = $(this).scrollTop();
+    let opacity = 1 - (scroll / 470);
+    if (opacity >= 0) {
+        $('.banner__logo').css('opacity', opacity);
     }
-  });
+});
 });
