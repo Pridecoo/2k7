7175087641b15474bbea30577f0df23d6572d4ff
Pridecoo <pridecoo@gmail.com>
2020-09-17 14:30:52 +0300

    all done

--- build/js/main.js
+++ build/js/main.js
@@ -1,44 +1,74 @@
 $(function () {
 
   // Smooth Scroll
-  $("[data-scroll]").on("click", function(event) {
-      event.preventDefault();
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
 
-      var $this = $(this),
-          blockId = $this.data("scroll"),
-          blockOffset = $(blockId).offset().top - 80;
-      
-      $("#nav a").removeClass("active");
-      $this.addClass("active");
+    var $this = $(this),
+      blockId = $this.data("scroll"),
+      blockOffset = $(blockId).offset().top - 80;
 
-      $("html, body").animate({
-          scrollTop: blockOffset
-      }, 500);
+    $("#nav a").removeClass("active");
+    $this.addClass("active");
+
+    $("html, body").animate({
+      scrollTop: blockOffset
+    }, 500);
   });
 
 
-  // $(window).scroll(() => { 
-  //   // Distance from top of document to top of footer.
-  //   topOfFooter = $('.footer').position().top;
-  //   // Distance user has scrolled from top, adjusted to take in height of sidebar (570 pixels inc. padding).
-  //   scrollDistanceFromTopOfDoc = $(document).scrollTop() + 570;
-  //   // Difference between the two.
-  //   scrollDistanceFromTopOfFooter = scrollDistanceFromTopOfDoc - topOfFooter;
-  
-  //   // If user has scrolled further than footer,
-  //   // pull sidebar up using a negative margin.
-  //   if (scrollDistanceFromTopOfDoc > topOfFooter) {
-  //     $('.banner__logo').css('margin-top',  0 - scrollDistanceFromTopOfFooter);
-  //   } else  {
-  //     $('.banner__logo').css('margin-top', 0);
-  //   }
-  // });
+  $(window).scroll(() => {
+    // Distance from top of document to top of footer.
+    topOfFooter = $('.footer').position().top;
+    // Distance user has scrolled from top, adjusted to take in height of sidebar (570 pixels inc. padding).
+    scrollDistanceFromTopOfDoc = $(document).scrollTop() + 570;
+    // Difference between the two.
+    scrollDistanceFromTopOfFooter = scrollDistanceFromTopOfDoc - topOfFooter;
 
-  $(window).scroll(function(event) {
+    // If user has scrolled further than footer,
+    // pull sidebar up using a negative margin.
+    if (scrollDistanceFromTopOfDoc > topOfFooter) {
+      $('.banner__logo').css('margin-top', 0 - scrollDistanceFromTopOfFooter);
+    } else {
+      $('.banner__logo').css('margin-top', 0);
+    }
+  });
+
+  $(window).scroll(function (event) {
     let scroll = $(this).scrollTop();
     let opacity = 1 - (scroll / 470);
     if (opacity >= 0) {
-        $('.banner__logo').css('opacity', opacity);
+      $('.banner__logo').css('opacity', opacity);
     }
+  });
+
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+    clickable: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true
+    },
+    speed: 1000,
+    spaceBetween: 100
+  });
+
+  $(".swiper-container").hover(function () {
+    (this).swiper.autoplay.stop();
+  }, function () {
+    (this).swiper.autoplay.start();
+  });
+
+  $(".hamburger").click(function () {
+    $(".menu").toggleClass("visible");
+    $(".hamburger").toggleClass("is-active");
+    $(".logo").toggleClass("right");
+  });
 });
-});
+
--- gulpfile.js
+++ gulpfile.js
@@ -31,11 +31,11 @@ gulp.task("sass", function () {
 
 gulp.task("scripts", function () {
   return gulp.src([
-      "node_modules/slick-carousel/slick/slick.js",
-      "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
-      "node_modules/mixitup/dist/mixitup.js",
-      "node_modules/rateyo/src/jquery.rateyo.js",
-      "node_modules/ion-rangeslider/js/ion.rangeSlider.js"
+      // "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
+      // "node_modules/mixitup/dist/mixitup.js",
+      // "node_modules/rateyo/src/jquery.rateyo.js",
+      // "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
+      "node_modules/swiper/swiper-bundle.js"
     ])
     .pipe(concat("libs.min.js"))
     .pipe(uglify())
@@ -46,10 +46,11 @@ gulp.task("scripts", function () {
 gulp.task("styles", function () {
   return gulp.src([
       "node_modules/normalize.css/normalize.css",
-      "node_modules/rateyo/src/jquery.rateyo.css",
-      "node_modules/slick-carousel/slick/slick.css",
-      "node_modules/magnific-popup/dist/magnific-popup.css",
-      "node_modules/ion-rangeslider/css/ion.rangeSlider.css"
+      // "node_modules/rateyo/src/jquery.rateyo.css",
+      // "node_modules/magnific-popup/dist/magnific-popup.css",
+      // "node_modules/ion-rangeslider/css/ion.rangeSlider.css",
+      "node_modules/swiper/swiper-bundle.css",
+      "node_modules/hamburgers/dist/hamburgers.css",
     ])
     .pipe(concat("libs.min.css"))
     .pipe(cssmin())
